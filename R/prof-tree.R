54177af7788aeae251d398685c0a3247ebdf6aa4
Artem Klevtsov <a.a.klevtsov@gmail.com>
2016-01-16 18:03:39 +0700

    Init

---
+++ R/prof-tree.R
@@ -0,0 +1,51 @@
+parse_log <- function(filename) {
+    log <- scan(filename, what = "character", quote = "\"", sep = "\n",
+                strip.white = TRUE, multi.line = FALSE, quiet = TRUE)
+    if (length(log) == 1L)
+        stop(sprintf("'%s' file is empty.", filename))
+    interval <- as.numeric(strsplit(log[1L],split = "=", fixed = TRUE)[[1L]][2L]) / 1e06
+    log <- log[-1L]
+    log <- log[!grepl("^#", log)]
+    calls <- unique(log)
+    real.time <- tabulate(match(log, calls)) * interval
+    total.time <- sum(real.time)
+    pct.time <- real.time / total.time
+    calls <- lapply(strsplit(calls, split = " ", fixed = TRUE), rev)
+    calls <- vapply(calls, function(x) paste(c("calls", x), collapse = "/"), character(1L))
+    data.frame(pathString = calls, real = real.time, percent = pct.time, stringsAsFactors = FALSE)
+}
+
+fun_env <- function(f) {
+    envs <- c(.GlobalEnv, lapply(loadedNamespaces(), .getNamespace))
+    for (env in envs) {
+        if (exists(f, env, inherits = FALSE))
+            return(environmentName(env))
+    }
+    return(NA)
+}
+
+#' @title Summarise Output of R Sampling Profiler
+#' @description Summarise the output of the Rprof function to show the amount of time used by different R functions.
+#' @param filename Name of a file produced by Rprof().
+#' @importFrom data.tree FromDataFrameTable isNotRoot isNotLeaf Aggregate SetFormat FormatPercent
+#' @export
+prof.tree <- function(filename = "Rprof.out") {
+    calls <- parse_log(filename)
+    tree <- FromDataFrameTable(calls)
+    tree$Set(real = 0, filterFun = function(node) is.null(node$real))
+    tree$Set(percent = 0, filterFun = function(node) is.null(node$percent))
+    tree$Do(function(node) node$real <- node$real + Aggregate(node, "real", sum),
+            traversal = "post-order", filterFun = isNotLeaf)
+    tree$Do(function(node) node$percent <- node$percent + Aggregate(node, "percent", sum),
+            traversal = "post-order", filterFun = isNotLeaf)
+    tree$Do(function(node) node$env <- fun_env(node$name), filterFun = isNotRoot)
+    SetFormat(tree, "percent", function(x) FormatPercent(x, digits = 2))
+    class(tree) <- c("ProfTree", class(tree))
+    return(tree)
+}
+
+#' @importFrom data.tree ToDataFrameTree
+#' @export
+print.ProfTree <- function(x, ...) {
+    print(ToDataFrameTree(x, "real", "percent", "env"))
+}
---
+++ demo/prof.R
@@ -0,0 +1,17 @@
+library(prof.tree)
+
+log <- tempfile(fileext = ".log")
+x <- runif(10e5)
+f <- function(x) c(mean(x), median(x), sd(x))
+f2 <- function(x) {
+    c(sum(x) / length(x),
+      median.default(x),
+      sqrt(sum((x - sum(x) / length(x))^2L) / (length(x) - 1L)))
+}
+Rprof(log, interval = 0.01)
+for (i in 1:100) f(x)
+for (i in 1:100) f2(x)
+Rprof(NULL)
+
+tree <- prof.tree(log)
+tree
