667f1701491d68ff982a6e4a291a83049548f136
Artem Klevtsov <a.a.klevtsov@gmail.com>
2016-01-17 22:57:54 +0700

    Code format in the demo

--- demo/prof.R
+++ demo/prof.R
@@ -1,5 +1,3 @@
-library(prof.tree)
-tmp <- tempfile(fileext = ".log")
 x <- runif(10e5)
 f <- function(x) {
     c(mean(x), median(x), sd(x))
@@ -7,10 +5,11 @@ f <- function(x) {
 f2 <- function(x) {
     c(sum(x) / length(x), median.default(x), sqrt(sum((x - sum(x) / length(x))^2L) / (length(x) - 1L)))
 }
-Rprof(tmp, interval = 0.01)
+Rprof(tmp <- tempfile(fileext = ".log"), interval = 0.01)
 for (i in 1:100) f(x)
 for (i in 1:100) f2(x)
 Rprof(NULL)
+library(prof.tree)
 tree <- prof.tree(tmp)
 tree
 unlink(tmp)
