bb4b07196436266b2f6eb6e444b9b70ce73f8beb
Artem Klevtsov <a.a.klevtsov@gmail.com>
2016-01-19 03:18:24 +0700

    Fix time calculation in ther parse_log

--- R/parse.R
+++ R/parse.R
@@ -7,11 +7,13 @@ parse_log <- function(filename) {
     proflog <- proflog[-1L]
     interval <- as.numeric(strsplit(first, "=", fixed = TRUE)[[1L]][2L]) / 1e06
     calls <- unique(proflog, fromLast = TRUE)
-    calls <- remove_extra_info(calls, first)
-    calls <- remove_source_frame(calls)
+    if (grepl("line profiling", first, fixed = TRUE))
+        calls <- calls[!grepl("#File ", calls, fixed = TRUE)]
     real.time <- tabulate(match(proflog, calls)) * interval
     total.time <- sum(real.time)
     pct.time <- real.time / total.time
+    calls <- remove_extra_info(calls, first)
+    calls <- remove_source_frame(calls)
     calls <- lapply(strsplit(calls, split = " ", fixed = TRUE), rev)
     calls <- vapply(calls, function(x) paste(c("calls", x), collapse = "/"), character(1L))
     data.frame(pathString = calls, real = real.time, percent = pct.time, stringsAsFactors = FALSE)
@@ -25,13 +27,21 @@ remove_source_frame <- function(calls) {
     calls
 }
 
+remove_knitr_frame <- function(calls) {
+    pattern <- " process_file <Anonymous>$"
+    idx <- grepl(pattern, calls)
+    if (sum(idx) == length(calls)) {
+        calls <- sub(" evaluate_call <Anonymous> in_dir block_exec call_block process_group.block process_group withCallingHandlers process_file <Anonymous>$", "", calls)
+        calls <- sub(" eval eval withVisible withCallingHandlers doTryCatch tryCatchOne tryCatchList tryCatch try handle$", "", calls)
+    }
+    calls
+}
+
 remove_extra_info <- function(calls, meta) {
     if (!grepl("profiling", meta, fixed = TRUE))
         return(calls)
-    if (grepl("line profiling", meta, fixed = TRUE)) {
-        calls <- calls[!grepl("#File ", calls, fixed = TRUE)]
+    if (grepl("line profiling", meta, fixed = TRUE))
         calls <- gsub("\\d+#\\d+", "", calls)
-    }
     if (grepl("memory profiling", meta, fixed = TRUE))
         calls <- gsub("^:.*:", "", calls)
     if (grepl("GC profiling", meta, fixed = TRUE))
