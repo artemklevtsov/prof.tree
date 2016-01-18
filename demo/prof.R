706a78da5506d5f878e2f8e73550d5d33434ff49
Artem Klevtsov <a.a.klevtsov@gmail.com>
2016-01-19 03:17:56 +0700

    Change Rprof interval in the demo

--- demo/prof.R
+++ demo/prof.R
@@ -5,7 +5,7 @@ f <- function(x) {
 f2 <- function(x) {
     c(sum(x) / length(x), median.default(x), sqrt(sum((x - sum(x) / length(x))^2L) / (length(x) - 1L)))
 }
-Rprof(tmp <- tempfile(fileext = ".log"), interval = 0.01)
+Rprof(tmp <- tempfile(fileext = ".log"), interval = 0.001)
 for (i in 1:100) f(x)
 for (i in 1:100) f2(x)
 Rprof(NULL)
